06bfb0398230baf879f10df73847d32d2f41a34f
Dean Martin <contact@deanmartin.pro>
2019-03-07 01:56:57 -0500

    Updated default log filename

--- cmd/laverna-cli/main.go
+++ cmd/laverna-cli/main.go
@@ -37,7 +37,7 @@ func downloadBook(url string) {
 }
 
 func main() {
-	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
